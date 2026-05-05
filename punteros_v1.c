17c4a4a50836f497ce5e70b5a61509dd4e3d2a1e
Emily <emily.quelal.gonzalez@udla.edu.ec>
2026-05-05 11:47:46 -0500

    Primer commit

---
+++ punteros_v1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num=5, a;
+    int *x; 
+    x = &num; //a x se interpreta 
+    a = *x; 
+    printf("%p %d",x,a);
+
+    char letra= 'a', c; 
+    char *ptr;
+    ptr = &letra; 
+    c = *ptr; 
+    printf ("%p %c", ptr, c);
+
+    return 0;
+}
